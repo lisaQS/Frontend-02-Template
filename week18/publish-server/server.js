f64a4efed8da266b779e08b70d85c199cda3f798
Lisa Shi <qinhang.shi@kuka.com>
2020-11-09 00:25:55 +0800

    upload
    upload

---
+++ week18/publish-server/server.js
@@ -0,0 +1,6 @@
+let http = require('http');
+let unzipper = require('unzipper');
+
+http.createServer(function(req, res) {
+    req.pipe(unzipper.Extract({path: '../server/public/'}));
+}).listen(8082);
